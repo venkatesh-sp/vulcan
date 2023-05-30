d26ac0fb1d4c99370cdb4b2677a37614d2ce4e97
venkatesh-sp <venkatesh.marreboyina@soulpageit.com>
2023-05-30 13:43:53 +0530

    feat: converts hl7 v2 to hl7 fhir

--- routes/message_queue.py
+++ routes/message_queue.py
@@ -4,9 +4,13 @@ import json
 import asyncio
 
 from dotenv import load_dotenv
-from pydantic import BaseModel
 from aio_pika import connect, Message, IncomingMessage
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
+
+from fhir.resources.patient import Patient
+from fhir.resources.identifier import Identifier
+from fhir.resources.humanname import HumanName
+from hl7apy import parser
 
 
 router = APIRouter()
@@ -21,17 +25,12 @@ amqp_user = os.getenv("AMQP_USER", "guest")
 amqp_password = os.getenv("AMQP_PASSWORD", "guest")
 
 
-class IMessage(BaseModel):
-    text: str
-
-
 message_queue = asyncio.Queue()
 
 
 async def on_message(message: IncomingMessage):
     print(message)
     txt = message.body.decode("utf-8")
-    txt = json.loads(txt)
     message_queue.put_nowait({"message_id": message.message_id, "message": txt})
 
 
@@ -74,13 +73,13 @@ async def get_message_by_id(message_id):
     # Return the found message or None if not found
     for message in messages:
         if message["message_id"] == message_id:
-            return message
+            return message["message"]
 
     return None
 
 
 @router.post("/produce")
-async def produce_message(message: IMessage):
+async def produce_message(file: UploadFile):
     # Publish a message to RabbitMQ
     message_id = uuid.uuid4().hex
 
@@ -94,8 +93,10 @@ async def produce_message(message: IMessage):
 
     channel = await connection.channel()
 
+    message = await file.read()
+
     await channel.default_exchange.publish(
-        Message(json.dumps(message.text).encode("utf-8"), message_id=message_id),
+        Message(message, message_id=message_id),
         routing_key=amqp_queue,
     )
 
@@ -103,10 +104,36 @@ async def produce_message(message: IMessage):
     return {"message": "Message sent to the queue", "message_id": message_id}
 
 
+def parse_message(msg):
+    msg = parser.parse_message(
+        msg.replace("\n", "\r"), find_groups=False, validation_level=2
+    )
+    # Extract relevant data from HL7 v2 message
+    patient_id = msg.PID.PID_3.CX_1.value
+    patient_name = msg.PID.PID_5.XPN_1.value
+    birth_date = msg.PID.PID_7.value
+
+    # Create FHIR resources
+    patient = Patient()
+    patient.id = patient_id
+    patient.name = [HumanName(text=patient_name)]
+    patient.birthDate = birth_date
+
+    identifier = Identifier()
+    identifier.system = "http://example.com/mrn"
+    identifier.value = patient_id
+    patient.identifier = [identifier]
+
+    # Convert FHIR resources to JSON
+    fhir_json = patient.json(indent=4)
+
+    return fhir_json
+
+
 @router.get("/consume/{message_id}")
 async def consume_message(message_id):
     message = await get_message_by_id(message_id)
-    if message is not None:
-        return {"message": message["message"]}
-    else:
+    if not message:
         return {"message": "No message found with the given id"}
+    fhir_json = parse_message(message)
+    return json.loads(fhir_json)
